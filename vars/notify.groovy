6d51054b7d59e2daa0db5f69b4093c53c0387e9f
nksinghfirst <117273440+nksinghfirst@users.noreply.github.com>
2024-08-22 19:59:56 +0530

    Update notify.groovy

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -7,12 +7,14 @@ def call(Map params = [:]) {
         from: params.from ?: 'alert@stacknize.com',
         to: params.to ?: 'nksinghfirst@gmail.com',
         subject: params.subject ?: "Build ${currentBuild.result} for I-Heal",
-        body: params.body ?: """Build status: ${currentBuild.result}.
-        Please check the logs for more details.
-        Job: ${JOB_NAME}
-        Build Number: ${BUILD_NUMBER}
-        Build URL: ${BUILD_URL}
+        body: params.body ?: """
+            Build status: ${currentBuild.result}.
+            Please check the logs for more details.
+            Job: ${JOB_NAME}
+            Build Number: ${BUILD_NUMBER}
+            Build URL: ${BUILD_URL}
         """,
         attachLog: params.attachLog ?: true
     )
 }
+

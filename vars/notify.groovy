9a1917dad46e874c98d18967eb593bbcd27a959c
nksinghfirst <117273440+nksinghfirst@users.noreply.github.com>
2024-08-22 20:58:31 +0530

    Update notify.groovy

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -2,38 +2,18 @@
 //    emailext attachLog: true, body: 'test', subject: 'Build for this has started', to: 'nksinghfirst@gmail.com'
 //}
 
-//def call(Map params = [:]) {
-//    emailext(
-//        from: params.from ?: 'alert@stacknize.com',
-//        to: params.to ?: 'nksinghfirst@gmail.com',
-//        subject: params.subject ?: "Build ${currentBuild.result} for I-Heal",
-//        body: params.body ?: """
-//            Build status: ${currentBuild.result}.
-//            Please check the logs for more details.
-//            Job: ${JOB_NAME}
-//            Build Number: ${BUILD_NUMBER}
-//            Build URL: ${BUILD_URL}
-//        """,
-//        attachLog: params.attachLog ?: true
-//    )
-//}
-
 def call(Map params = [:]) {
-    // Set default subject and body if not provided
-    String defaultSubject = "Build ${currentBuild.result} for I-Heal"
-    String defaultBody = """
-        Build status: ${currentBuild.result}.
-        Please check the logs for more details.
-        Job: ${JOB_NAME}
-        Build Number: ${BUILD_NUMBER}
-        Build URL: ${BUILD_URL}
-    """
-    
     emailext(
         from: params.from ?: 'alert@stacknize.com',
         to: params.to ?: 'nksinghfirst@gmail.com',
-        subject: params.subject ?: defaultSubject,
-        body: params.body ?: defaultBody,
+        subject: params.subject ?: "Build ${currentBuild.result} for I-Heal",
+        body: params.body ?: """
+            Build status: ${currentBuild.result}.
+            Please check the logs for more details.
+            Job: ${JOB_NAME}
+            Build Number: ${BUILD_NUMBER}
+            Build URL: ${BUILD_URL}
+        """,
         attachLog: params.attachLog ?: true
     )
 }

239f0f2a6a2600d597f4a3431868fa14b25730c7
nksinghfirst <117273440+nksinghfirst@users.noreply.github.com>
2024-08-22 21:08:01 +0530

    Update notify.groovy

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -2,18 +2,25 @@
 //    emailext attachLog: true, body: 'test', subject: 'Build for this has started', to: 'nksinghfirst@gmail.com'
 //}
 
+
+
 def call(Map params = [:]) {
+    // Hardcode the default subject and body
+    String defaultSubject = "Build ${currentBuild.result} for I-Heal"
+    String defaultBody = """
+        Build status: ${currentBuild.result}.
+        Please check the logs for more details.
+        Job: ${JOB_NAME}
+        Build Number: ${BUILD_NUMBER}
+        Build URL: ${BUILD_URL}
+    """
+    
+    // Use the default values if none are provided
     emailext(
-        from: params.from ?: 'alert@stacknize.com',
-        to: params.to ?: 'nksinghfirst@gmail.com',
-        subject: params.subject ?: "Build ${currentBuild.result} for I-Heal",
-        body: params.body ?: """
-            Build status: ${currentBuild.result}.
-            Please check the logs for more details.
-            Job: ${JOB_NAME}
-            Build Number: ${BUILD_NUMBER}
-            Build URL: ${BUILD_URL}
-        """,
-        attachLog: params.attachLog ?: true
+        from: params.get('from', 'alert@stacknize.com'),
+        to: params.get('to', 'nksinghfirst@gmail.com'),
+        subject: params.get('subject', defaultSubject),
+        body: params.get('body', defaultBody),
+        attachLog: params.get('attachLog', true)
     )
 }

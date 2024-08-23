2c3da3ed1e887d37146cbaad5e0d35bc9b3a8633
nksinghfirst <117273440+nksinghfirst@users.noreply.github.com>
2024-08-23 11:06:18 +0530

    Update notify.groovy

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -1,13 +1,13 @@
 def call(Map params = [:]) {
     emailext(
-        to: params.to ?: 'nksinghfirst@gmail.com',
-        subject: params.subject ?: "Build ${currentBuild.result} for I-Heal",
+        to: 'nksinghfirst@gmail.com',
+        subject: params.subject ?: "",
         body: params.body ?: """
             Build status: ${currentBuild.result}. Please check the logs for more details.
-        Job: ${JOB_NAME}
-        Build Number: ${BUILD_NUMBER}
-        Build URL: ${BUILD_URL}
-        """,
-        attachLog: params.attachLog ?: true
+Job: ${JOB_NAME}
+Build Number: ${BUILD_NUMBER}
+Build URL: ${BUILD_URL}
+""",
+        attachLog: true
     )
 }

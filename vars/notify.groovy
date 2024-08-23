42eb8c0b27386858ee61b5b5368d77b8eabe4828
nksinghfirst <117273440+nksinghfirst@users.noreply.github.com>
2024-08-23 11:10:42 +0530

    Update notify.groovy

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -1,7 +1,7 @@
 def call(Map params = [:]) {
     emailext(
         to: 'nksinghfirst@gmail.com',
-        subject: params.subject ?: "",
+        subject: params.subject ?: "Build ${currentBuild.result} for ${env.FOLDER_NAME}",
         body: params.body ?: """
             Build status: ${currentBuild.result}. Please check the logs for more details.
 Job: ${JOB_NAME}

aa5de3bd408ed80e279c5ee432021f12e982651b
nksinghfirst <117273440+nksinghfirst@users.noreply.github.com>
2024-08-26 12:38:15 +0530

    Update notify.groovy

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -10,7 +10,6 @@ Build Number: ${BUILD_NUMBER}
 Build URL: ${BUILD_URL}
         """,
         subject: params.subject ?: "Build ${currentBuild.result} for ${folderName}",
-        to: params.to ?: 'nksinghfirst@gmail.com',
-        from: params.from ?: 'alert@stacknize.com'
+        to: params.to ?: 'nksinghfirst@gmail.com'
     )
 }

ab8962fd5cb289969631f4642864da5422051c8b
nksinghfirst <117273440+nksinghfirst@users.noreply.github.com>
2024-08-26 12:15:42 +0530

    Update notify.groovy

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -1,30 +1,17 @@
 def call(Map params = [:]) {
-    // Extract the folder name from JOB_NAME
+    // Extract folder name from JOB_NAME
     def folderName = env.JOB_NAME.split('/')[0]
-
-    // Default email parameters
-    def defaultParams = [
-        attachLog: true,
-        body: """Build status: ${currentBuild.result}.
-Please check the logs for more details.
-Job: ${env.JOB_NAME}
-Folder: ${folderName}
-Build Number: ${env.BUILD_NUMBER}
-Build URL: ${env.BUILD_URL}
-        """,
-        subject: "Build ${currentBuild.result} for ${folderName}",
-        to: 'nksinghfirst@gmail.com'
-    ]
-
-    // Merge provided params with defaults
-    def emailParams = defaultParams + params
-
-    // Send the email
+    
     emailext(
-        attachLog: emailParams.attachLog,
-        body: emailParams.body,
-        subject: emailParams.subject,
-        to: emailParams.to
+        attachLog: params.attachLog ?: true,
+        body: params.body ?: """Build status: ${currentBuild.result}. Please check the logs for more details.
+Job: ${JOB_NAME}
+Folder: ${folderName}
+Build Number: ${BUILD_NUMBER}
+Build URL: ${BUILD_URL}
+        """,
+        subject: params.subject ?: "Build ${currentBuild.result} for ${folderName}",
+        to: params.to ?: 'nksinghfirst@gmail.com',
+        from: params.from ?: 'alert@stacknize.com'
     )
 }
-
